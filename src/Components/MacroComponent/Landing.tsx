9ba1c8daa55e629468bf7db0b8db60150874c733
VishwaTechnologiesPvtLtd <151992843+VishwaTechnologiesPvtLtd@users.noreply.github.com>
2024-08-07 09:16:40 +0530

    Router browser added

--- src/Components/MacroComponent/Landing.tsx
+++ src/Components/MacroComponent/Landing.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import SideMenu from '../SideMenu/SideMenu.tsx'
 import DashBoard from './DashBoard.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from '../Pages/User.tsx';
 
 export default function Landing() {
   return (
+    <BrowserRouter>
     <div  style={{ display: 'grid', gridTemplateColumns: '15% 1fr', height: '100vh' }}>
     <div  style={{ backgroundColor: '#062a5a' }}>
       <SideMenu />
     </div>
-    <div className="dashboard">
+    <Routes>
+    <Route path="/" element={<div className="dashboard">
       <DashBoard />
-    </div>
+    </div>} />
+
+    <Route path="User" element={<User />} />
+    
+    </Routes>
   </div>
+  </BrowserRouter>
   )
 }
--- src/Components/SideMenu/SideMenu.tsx
+++ src/Components/SideMenu/SideMenu.tsx
@@ -1,6 +1,8 @@
+import { Outlet, Link } from "react-router-dom";
 import React, { useState } from 'react'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../../css/sidemenu.css'
+
 
 export default function SideMenu() {
    
@@ -9,7 +11,8 @@ export default function SideMenu() {
     <div style={{backgroundColor: '#062a5a',height:"100vh", position: 'fixed'}}>
         <div className='side-bar-menu-font'>
         MENU
-        <div className='side-bar-menu-item'>&nbsp;&nbsp;<i className='bi bi-boxes side-bar-icon'></i>&nbsp;&nbsp;&nbsp;Dashboard</div>
+        <Link className="linkto" to="/"><div className='side-bar-menu-item'>&nbsp;&nbsp;<i className='bi bi-boxes side-bar-icon'></i>&nbsp;&nbsp;&nbsp;Dashboard</div></Link>
+        <Link className="linkto" to="User"><div className='side-bar-menu-item'>&nbsp;&nbsp;<i className='bi bi-person side-bar-icon'></i>&nbsp;&nbsp;&nbsp;Users</div></Link>
         <div className='side-bar-menu-item'>&nbsp;&nbsp;<i className='bi bi-building side-bar-icon'></i>&nbsp;&nbsp;&nbsp;Property</div>
         <div className='side-bar-menu-item'>&nbsp;&nbsp;<i className='bi bi-currency-rupee side-bar-icon'></i>&nbsp;&nbsp;&nbsp;Transaction</div>
         <div className='side-bar-menu-item'>&nbsp;&nbsp;<i className='bi bi-file-diff-fill side-bar-icon'></i>&nbsp;&nbsp;&nbsp;Reports</div>
